4d0c06f20b975fa73953cf0b9c3b7afb2e1e40e8
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-03-03 21:27:52 -0500

    imported axios and fetched data from NASA APOD API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
 
+
+  
 function App() {
+  
+  axios.get("https://api.nasa.gov/planetary/apod?api_key=KIgeE6B1p5RrbH06R2C7yYeHZFGIuCFKPtbpXLrQ&date=2012-03-14")
+  .then (response => {
+    console.log(response)
+
+  
+  })
+  .catch (error => {
+    return "There was an error retrieving the data"
+  })
+
   return (
+  
+  
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+     
     </div>
   );
 }
