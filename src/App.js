5062b91ced63877abc8e75ffaaac6d4296ebb9d5
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-04-30 18:53:52 -0400

    added calendar component and styling, lacks functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from "react";
-
+import Calendar from "react-calendar";
 import "./App.css";
 import ImageContainer from "./components/ImageContainer";
 import ImageInfo from "./components/ImageInfo";
 
-  
 function App() {
-  
-  
   return (
-  
-    
     <div className="App">
-      <ImageContainer  />
+      <div className="Calendar">
+        <p>Choose A Date</p>
+        <Calendar />
+      </div>
+      <ImageContainer />
       <ImageInfo />
     </div>
-  )
-
-
-  }
-  
-
-
-
+  );
+}
 
 export default App;
